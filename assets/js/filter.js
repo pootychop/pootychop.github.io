ca9333646702da0789b34fd6a22b6a4535e239f2
Julianna Beckert <julianna.beckert@gmail.com>
2019-04-15 15:02:17 -0600

    add home page content and normalize css

---
+++ assets/js/filter.js
@@ -0,0 +1,12 @@
+$(function() {
+		var selectedClass = "";
+		$(".fil-cat").click(function(){
+		selectedClass = $(this).attr("data-rel");
+     $("#portfolio").fadeTo(100, 0.1);
+		$("#portfolio div.item").not("."+selectedClass).fadeOut().removeClass('scale-anm');
+    setTimeout(function() {
+      $("."+selectedClass).fadeIn().addClass('scale-anm');
+      $("#portfolio").fadeTo(300, 1);
+    }, 300);
+	});
+});
